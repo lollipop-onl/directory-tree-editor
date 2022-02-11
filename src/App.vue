dee88fd28c68bb3c5016e1d03fbd207a91b444ef
lollipop-onl <lollipop@simochee.net>
2022-02-11 14:43:29 +0900

    🔧 add app version

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script lang="ts" setup>
 const name = 'simochee';
+const appVersion = `${__APP_VERSION__}`;
 </script>
 
 <template>
+  <img src="@/assets/logo.svg" alt="Directory Tree Editor" />
+  <p>v{{ appVersion }}</p>
   <h1 class="mt-4 bg-red-100">Hello, {{ name }}</h1>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
+  define: {
+    __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+  },
   plugins: [vue()],
 });
