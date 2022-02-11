b1adf1789dfd1bb1fc339709cbda9c67f17f9afe
lollipop-onl <lollipop@simochee.net>
2022-02-11 22:28:43 +0900

    ✨ add GTM

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import 'tailwindcss/tailwind.css';
 import { createApp } from 'vue';
+import { createGtm } from '@gtm-support/vue-gtm';
 import App from '@/App.vue';
 
-createApp(App).mount('#root');
+const app = createApp(App);
+
+app.use(
+  createGtm({
+    id: 'GTM-KQJBKCN',
+  })
+);
+
+app.mount('#root');
 
 export {};
